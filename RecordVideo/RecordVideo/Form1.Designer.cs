04d5f96e5d1fe2cc13a3bb0adc1b1b5e5c7541b1
Cosydays <jiahui_cai@yahoo.com>
2018-08-17 22:54:43 +0800

    first push

---
+++ RecordVideo/RecordVideo/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace RecordVideo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.录制视频ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.停止录制ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.录制视频ToolStripMenuItem,
+            this.停止录制ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(684, 29);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 录制视频ToolStripMenuItem
+            // 
+            this.录制视频ToolStripMenuItem.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.录制视频ToolStripMenuItem.Name = "录制视频ToolStripMenuItem";
+            this.录制视频ToolStripMenuItem.Size = new System.Drawing.Size(86, 25);
+            this.录制视频ToolStripMenuItem.Text = "录制视频";
+            this.录制视频ToolStripMenuItem.Click += new System.EventHandler(this.录制视频ToolStripMenuItem_Click);
+            // 
+            // 停止录制ToolStripMenuItem
+            // 
+            this.停止录制ToolStripMenuItem.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.停止录制ToolStripMenuItem.Name = "停止录制ToolStripMenuItem";
+            this.停止录制ToolStripMenuItem.Size = new System.Drawing.Size(86, 25);
+            this.停止录制ToolStripMenuItem.Text = "停止录制";
+            this.停止录制ToolStripMenuItem.Click += new System.EventHandler(this.停止录制ToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 328);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 录制视频ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 停止录制ToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ RecordVideo/RecordVideo/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ESBasic;
+using Oraycn.MCapture;
+using Oraycn.MFile;
+
+namespace RecordVideo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 录制视频ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int audioSampleRate = 16000;
+                int channelCount = 1;
+                System.Drawing.Size videoSize = Screen.PrimaryScreen.Bounds.Size;
+
+                {
+                    videoSize = new System.Drawing.Size(640, 480);
+                    this.cameraCapturer = CapturerFactory.CreateCameraCapturer(0, videoSize, frameRate);
+                    this.cameraCapturer.ImageCaptured += new CbGeneric<Bitmap>(this.Form1_ImageCaptured);
+                }
+
+
+                {
+                    this.microphoneCapturer = CapturerFactory.CreateMicrophoneCapturer(0);
+                    this.microphoneCapturer.CaptureError += new CbGeneric<Exception>(this.CaptureError);
+                }
+
+                this.microphoneCapturer.AudioCaptured += audioMixter_AudioMixed;
+
+                this.microphoneCapturer.Start();
+
+                this.cameraCapturer.Start();
+
+                this.sizeRevised = (videoSize.Width % 4 != 0) || (videoSize.Height % 4 != 0);
+                if (this.sizeRevised)
+                {
+                    videoSize = new System.Drawing.Size(videoSize.Width / 4 * 4, videoSize.Height / 4 * 4);
+                }
+
+                this.videoFileMaker = new VideoFileMaker();
+                this.videoFileMaker.Initialize(video_save_path, VideoCodecType.H264, videoSize.Width, videoSize.Height, frameRate, VideoQuality.High, AudioCodecType.AAC, audioSampleRate, channelCount, true);
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+            }
+        }
+
+        private void 停止录制ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.microphoneCapturer.Stop();
+            this.cameraCapturer.Stop();
+            videoFileMaker.Close(true);
+            MessageBox.Show("录制完成！");
+        }
+
+        void CaptureError(Exception obj)
+        {
+
+        }
+
+        void Form1_ImageCaptured(Bitmap img)
+        {
+            Bitmap imgRecorded = img;
+            if (this.sizeRevised) // 对图像进行裁剪，  MFile要求录制的视频帧的长和宽必须是4的整数倍。
+            {
+                imgRecorded = ESBasic.Helpers.ImageHelper.RoundSizeByNumber(img, 4);
+                img.Dispose();
+            }
+            this.pictureBox1.Image = imgRecorded;
+            this.pictureBox1.Refresh();
+            this.videoFileMaker.AddVideoFrame(imgRecorded);
+        }
+
+        void audioMixter_AudioMixed(byte[] audioData)
+        {
+            this.videoFileMaker.AddAudioFrame(audioData);
+        }
+        private string video_save_path = "test.mp4";
+        private IMicrophoneCapturer microphoneCapturer;
+        private ICameraCapturer cameraCapturer;
+        private VideoFileMaker videoFileMaker;
+        private int frameRate = 15; // 采集视频的帧频
+        private bool sizeRevised = false;// 是否需要将图像帧的长宽裁剪为4的整数倍
+    }
+}
